307043605dca6b1e9894c90bc8fddbe9647cea47
godfrey-creat <ogodfrey135@gmail.com>
2023-03-09 01:51:51 -0800

    all possible different combinations of 2 digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(100):
     if int(i / 10) != i % 10 and int(i / 10) < i % 10:
         print("{}{}".format(int(i / 10), i % 10), end="")
-        if (i != 98):
+        if (i != 89):
             print(",", end="")
             print(" ")
