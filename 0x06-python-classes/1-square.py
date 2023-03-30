2bd39641c8f86a144646dca1976391ea1127a085
godfrey-creat <ogodfrey135@gmail.com>
2023-03-30 00:00:57 -0700

    class square that defines a square bases in 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Representing a square."""
+
+    def __init__(self, size):
+        """Initializing a new Square.
+        Args:
+            size (int): This is the size of the new square.
+        """
+        self.__size = size
