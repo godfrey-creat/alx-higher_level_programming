1cbc4a92a303ad52ffb4469e7ae447e8c1657174
godfrey-creat <ogodfrey135@gmail.com>
2023-04-13 02:53:18 -0700

    corecting that code

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-    """returning the dictionary representation of the object's instance variables"""
+    """returning the dict representation of the object's instance variables"""
     return obj.__dict__
