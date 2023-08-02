644eca20da0ae57db65a9c164fe0ff1912402a6b
godfrey-creat <ogodfrey135@gmail.com>
2023-08-02 07:24:14 -0700

    jQuery is made easier with javascript

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get("https://swapi-api.alx-tools.com/api/films/?format=json",
+      function (data, status) {
+	  console.log(data);
+          // const char = data;
+	  // $("DIV#character").text(char.name);
+      });
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $.getJSON("https://fourtonfish.com/hellosalut/?lang=fr",
+    function(data) {
+      var translation = data.hello;
+      $("#hello").text(traslation);
+    });
+  });
